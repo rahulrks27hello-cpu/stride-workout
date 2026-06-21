1884bad3a78b048e1bf511c5836c0543e3a84931
rahulrks27hello-cpu <rahulrks27.hello@gmail.com>
2026-06-21 19:46:24 +0530

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'stride-workout-v1';
+const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './pwa-192.svg', './pwa-512.svg'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))),
+    ),
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+
+      return fetch(event.request)
+        .then((response) => {
+          const clone = response.clone();
+          if (event.request.url.startsWith(self.location.origin)) {
+            caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+          }
+          return response;
+        })
+        .catch(() => caches.match('./index.html'));
+    }),
+  );
+});
